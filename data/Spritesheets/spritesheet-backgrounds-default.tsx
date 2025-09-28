cfe134d61353a7f25bfb6a1f4129a78fdebea258
Cr4ftingMine <gleitze.leon@fh-swf.de>
2025-09-28 18:15:24 +0200

    28.09 18:15 - Lösung des tsx-Problem beim klonen von Repo

--- data/Spritesheets/spritesheet-backgrounds-default.tsx
+++ data/Spritesheets/spritesheet-backgrounds-default.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="spritesheet-backgrounds-default" tilewidth="64" tileheight="64" tilecount="256" columns="16">
- <image source="../../../Tiles/kenney_new-platformer-pack-1.0/Spritesheets/spritesheet-backgrounds-default.png" width="1024" height="1024"/>
+ <image source="../../images/tilesets/spritesheet-backgrounds-default.png" width="1024" height="1024"/>
 </tileset>
--- data/Spritesheets/tilemap_scaled_64_64.tsx
+++ data/Spritesheets/tilemap_scaled_64_64.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tilemap_scaled_64_64" tilewidth="64" tileheight="64" tilecount="816" columns="34">
- <image source="../../../Tiles/input-prompts-pixel-16/Tilemap/tilemap_scaled_64_64.png" width="2176" height="1536"/>
+ <image source="../../images/tilesets/tilemap_scaled_64_64.png" width="2176" height="1536"/>
 </tileset>

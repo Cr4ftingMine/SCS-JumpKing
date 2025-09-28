36ae58739536a02817b45f0be4981f10fe9b3f77
Cr4ftingMine <gleitze.leon@fh-swf.de>
2025-09-28 16:02:25 +0200

    28.09 16:02 - Ergänzung von Infos bzgl Start

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 # Hinweis: Programm immer aus dem Projekt-Root starten,
 # sonst funktionieren die relativen Pfade zu "audio/" und "images/" nicht.
+# Das heißt:  
+# cd SCS-JumpKing
+# python code/main.py
 
 from menu import *
 
